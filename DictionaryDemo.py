4286bae9ba975bd6db88a5a2c681d357f83259e9
artishivaji <122979021+artishivaji@users.noreply.github.com>
2023-01-21 13:49:27 +0530

    Add files via upload

---
+++ DictionaryDemo.py
@@ -0,0 +1,30 @@
+print("Demonstration of Dictionary")
+    
+# Data : Heterogeneous
+#Ordered : Yes
+# Indexed : No
+# Mutable : Yes 
+# Duplicate : No
+
+programming = {"C" : "Ritchie", "C++" : "Stroustrup", "Java" : "Gosling" , "Python" : "Rossum", "C" : "Thompson"}
+Batches = {"PPA" : 18000, "LB" : 16700,"Python" : 16500, "Angular" : 15000}
+
+print("Data of Dictionary :",Batches)
+print("Data type is :",type(Batches))
+print("Length of dictionary :",len(Batches))
+print("Value of PPA is :",Batches["PPA"])
+
+print(programming)
+
+    
+    
+    
+    
+
+
+
+
+
+
+
+
---
+++ DictionaryIterate.py
@@ -0,0 +1,35 @@
+Batches = {"PPA" : 18000, "LB" : 16700, "Python" : 16500, "Angular" : 15000}
+print("Data of Dictionary :", Batches)
+
+print("Data traversal using for loop")
+for x in Batches:
+    print(x)
+
+print("Data traversal using for loop")
+for x in Batches:
+    print(x,Batches[x])
+
+print("Data traversal using for loop")
+for x in Batches:
+    print(x,Batches.get(x))
+
+
+keyBatches = Batches.keys()
+print(type(keyBathes))
+print(keyBatches)
+    
+
+valueBatches = Batches.values()
+print(type(valueBatches))
+print(valueBatches)
+
+    
+    
+
+
+
+
+
+
+
+
